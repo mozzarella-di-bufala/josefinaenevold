d1a0cea010b4ad78c3b111552704ddf39157e543
mozzarella-di-bufala <whirl-yardarm0o@icloud.com>
2023-08-08 23:39:19 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<main
+	class="max-w-2xl mx-auto min-h-screen p-8"
+	style="background-color: rgba(127, 140, 239, 0.75);"
+>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<h1 class="mb-8 text-4xl font-normal">Josefina Enevold</h1>
+<p class="mb-8">
+	Textile designer with an MFA from The Royal Academy, school of Design in Copenhagen
+</p>
+
+<h2>Ongoing exhibition</h2>
+<p class="mb-8">“Lovely Linen” at Køng Museum 17 July - 20 August 2023</p>
+
+<h2>To read about the process of making the textiles to the exhibition:</h2>
+<p class="mb-8 underline">
+	<a href="https://svfk.dk/project/josefina-enevold-koeng-museum"
+		>https://svfk.dk/project/josefina-enevold-koeng-museum</a
+	>
+</p>
+
+<h2>And if you want to see more work, go to my instagram.</h2>
+<p class="mb-8 underline">
+	<a href="https://www.instagram.com/josefinaenevold/">https://www.instagram.com/josefinaenevold/</a
+	>
+</p>
+
+<h3>To get in contact for request and collaborations please email</h3>
+<p class="underline">
+	<a href="mailto:info@josefinaenevold.com">info@josefinaenevold.com</a>
+</p>
